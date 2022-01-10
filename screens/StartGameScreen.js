acaa16033854f5d7c4a47a2d43165ea66a796aff
siddharth <jsidd06@gmail.com>
2022-01-10 13:44:00 +0530

    add card file with style

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import {View,StyleSheet,Text,TextInput,Button} from 'react-native';
+import Card from '../components/Card';
 
 const StartGameScreen = props => {
     return(
         <View style={styles.screen}>
             <Text style={styles.title}>Start Game Screen!</Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text >Select a Number</Text>
                 <TextInput />
                 <View style={styles.buttonContainer}>
                     <Button title="Reset" onPress={() => {}} />
                     <Button title="Confirm" onPress={() => {}} />
                 </View>
-            </View>
+            </Card>
         </View>
     );
 }
@@ -31,14 +32,6 @@ const styles = StyleSheet.create({
         width: 300,
         maxWidth: '80%',
         alignItems: 'center',
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 6,
-        elevation: 8,
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10,
     },
     buttonContainer:{
         flexDirection: 'row',
